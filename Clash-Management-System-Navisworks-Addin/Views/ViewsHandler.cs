66b2b965626c7ca1739cf7da92c4b4ed1fc1f383
Ahmed Abd Elbary <AhmedAElbary@gmail.com>
2021-06-25 12:00:18 +0200

    Update: WPF InProgress, adding sidebar

---
+++ Clash-Management-System-Navisworks-Addin/Views/ViewsHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clash_Management_System_Navisworks_Addin.Views
+{
+    public class ViewsHandler
+    { 
+        public void main()
+        {
+            MainWindow mw = new MainWindow();
+        }
+    }
+}
