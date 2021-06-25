2b7878c5169d623c1e16b5108099459885ef9d76
Ahmed Abd Elbary <AhmedAElbary@gmail.com>
2021-06-25 19:21:42 +0200

    Update: Views Handler Class updated

--- Clash-Management-System-Navisworks-Addin/Views/MainWindow.xaml.cs
+++ Clash-Management-System-Navisworks-Addin/Views/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -19,9 +21,28 @@ namespace Clash_Management_System_Navisworks_Addin.Views
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int expandedTrueHeight = 200;
+        public int expandedfalseHeight = 25;
+        public Brush expanderNormalBackground = (Brush)ColorConverter.ConvertFromString("MintCream");
+        public Brush expanderNormalForeground = (Brush)ColorConverter.ConvertFromString("LightSlateGray");
+        public Brush expanderHighlightBackground = (Brush)ColorConverter.ConvertFromString("MintCream");
+        public Brush expanderHighlightForeground = (Brush)ColorConverter.ConvertFromString("LightSlateGray");
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoginExpander_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Expander expander = sender as Expander;
+            FrameworkElement frameworkElement = e.OriginalSource as FrameworkElement;
+            if (frameworkElement is ToggleButton && frameworkElement.Name == "HeaderSite")
+            {
+                Trace.WriteLine("Clicked in expander header");
+                expander.IsEnabled = true;
+                expander.IsExpanded = true;
+                expander.Height = expandedTrueHeight;
+            }
         }
     }
 }
--- Clash-Management-System-Navisworks-Addin/Views/ViewsHandler.cs
+++ Clash-Management-System-Navisworks-Addin/Views/ViewsHandler.cs
@@ -3,14 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Clash_Management_System_Navisworks_Addin.ViewModels;
 
 namespace Clash_Management_System_Navisworks_Addin.Views
 {
-    public class ViewsHandler
-    { 
-        public void main()
+    public static class ViewsHandler
+    {
+        #region Static Properties
+        public static Credentials CurrentCredentials { get; set; }
+        public static User CurrentUser { get; set; }
+        public static Project CurrentProject { get; set; }
+        public static AClashMatrix CurrentAClashMatrix { get; set; }
+        public  static List<Project> CurrentUserProjects { get; set; }
+        public static List<AClashMatrix> CurrentProjectClashMatrix { get; set; }
+        #endregion
+
+        #region Static Methods
+        static bool WriteCredentialsToAssembly(string name,string password)
         {
-            MainWindow mw = new MainWindow();
+            throw new Exception("Method WriteCredentialsToAssembly: Work in progress");
+            return false;
         }
+
+        static bool WriteCurrentProjectNameToAssembly(string currentProjectName)
+        {
+            throw new Exception("Method WriteCurrentProjectNameToAssembly: Work in progress");
+            return false;
+        }
+
+        static bool WriteCurrentClashMatrixtNameToAssembly(string clashMatrixName)
+        {
+            throw new Exception("Method WriteCurrentClashMatrixtNameToAssembly: Work in progress");
+            return false;
+        }
+
+
+
+
+
+        #endregion
+
+
+
+
     }
 }
