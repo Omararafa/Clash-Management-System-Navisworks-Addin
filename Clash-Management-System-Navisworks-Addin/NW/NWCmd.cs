c28243b434149d0541ffe083750ae19a7384e807
Waelyassir <wael.yassir@gmail.com>
2021-06-26 11:20:18 +0200

    Edit ribbob command

--- Clash-Management-System-Navisworks-Addin/NW/NWCmd.cs
+++ Clash-Management-System-Navisworks-Addin/NW/NWCmd.cs
@@ -5,59 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Autodesk.Navisworks.Api.Plugins;
+using Clash_Management_System_Navisworks_Addin.Views;
 
 namespace Clash_Management_System_Navisworks_Addin
 {
-    #region Ribbon
-    //[Plugin("Clash-Management-System-Navisworks-Addin", "WYH", DisplayName = "Clash-Management-System-Navisworks-Addin")]
-    //[RibbonLayout("Clash-Management-System-Navisworks-Addin.xaml")]
-    //[RibbonTab("ID_CustomTab_1", DisplayName ="Hellooooooooo")]
-    //[Command("ID_Button_1")]
-    //public class NWDCmd : CommandHandlerPlugin
-    //{
-    //    public override int ExecuteCommand(string name, params string[] parameters)
-    //    {
-    //        // use button 1 to intiate the dock panel
-    //        switch (name)
-    //        {
-    //            case "ID_Button_1":
-    //                MessageBox.Show("Test");
-    //                break;
-    //        }
-
-    //        return 0;
-    //    }
-    //} 
-    #endregion
-
-
     [Plugin("Clash-Management-System-Navisworks-Addin", "WYHk", DisplayName = "Hello!")]
     [RibbonLayout("Clash-Management-System-Navisworks-Addin.xaml")]
     [RibbonTab("ID_CustomTab_1", DisplayName = "Clash Management")]
-    [Command("Clash-Management-System-Navisworks-Addin", CanToggle = true, DisplayName = "Clash Manager")]
+    [Command("BE", CanToggle = true, DisplayName = "Button1")]
+    [Command("WPFUI", CanToggle = true, DisplayName = "Button2")]
 
 
     class Clash_Management_System_Navisworks_Addin : CommandHandlerPlugin
     {
-        bool m_toEnableButton = true; // to enable button or not
         public override int ExecuteCommand(string commandId, params string[] parameters)
         {
-            
-
             switch (commandId)
             {
+                case "BE":
+                    MessageBox.Show("Button 1", "Hello from Button1!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
 
-                case "Clash-Management-System-Navisworks-Addin":
-                    {
-                        MessageBox.Show("Hello, Wael!", "Holla, it worked!", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-                        break;
-
-                    }
+                case "WPFUI":
+                    MainWindow mainMenu = new MainWindow();
+                    mainMenu.Show();
+                    break;
             }
 
             return 0;
         }
-
     }
 }
