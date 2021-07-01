111bc1ae9f57edebd87b0683b44b545b55218367
Waelyassir <wael.yassir@gmail.com>
2021-07-02 00:35:44 +0200

    add ReportHandler Class

---
+++ Clash-Management-System-Navisworks-Addin/Reporting/ReportHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Clash_Management_System_Navisworks_Addin.Reporting
+{
+    class ReportHandler
+    {
+    }
+}
