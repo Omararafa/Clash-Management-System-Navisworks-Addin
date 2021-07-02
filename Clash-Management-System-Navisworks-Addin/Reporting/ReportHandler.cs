d404aa4201be74f4efa6072f7569d96172d48df3
Waelyassir <wael.yassir@gmail.com>
2021-07-02 15:40:24 +0200

    Add report handler static method.

--- Clash-Management-System-Navisworks-Addin/Reporting/ReportHandler.cs
+++ Clash-Management-System-Navisworks-Addin/Reporting/ReportHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -6,7 +7,39 @@ using System.Collections.Generic;
 
 namespace Clash_Management_System_Navisworks_Addin.Reporting
 {
-    class ReportHandler
+    public static class ReportHandler
     {
+        #region Static Members
+        private static string _reportPath;
+
+        public static string ReprortPath
+        {
+            get { return _reportPath; }
+            set { _reportPath = value; }
+        }
+
+
+        #endregion
+
+        #region Reporting Methods
+
+        // --------------> THIS METHOD IS STIL IN PROGRESS <--------------
+        public static void WriteReport(List<Object> records, string reportPath)
+        {
+
+            try
+            {
+                using (StreamWriter streamWriter = new StreamWriter(reportPath, false))
+                {
+                    streamWriter.WriteLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("An error occurs, please retry!" + Environment.NewLine + ex.Message);
+            }
+        }
+
+        #endregion
     }
 }
