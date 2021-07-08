d442119609362e1cc608c7b54b0f4e63132471ab
Waelyassir <wael.yassir@gmail.com>
2021-07-08 23:17:59 +0200

    add AClashResult class memebers

--- Clash-Management-System-Navisworks-Addin/ViewModels/AClashTestResult.cs
+++ Clash-Management-System-Navisworks-Addin/ViewModels/AClashTestResult.cs
@@ -12,43 +12,77 @@ namespace Clash_Management_System_Navisworks_Addin.ViewModels
     public class AClashTestResult
     {
         public AClashTest ClashTest { get; set; }
-        public EntityComparisonResult Condition { get; set; }
+        public Guid Guid { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public string ApprovedBy { get; set; }
+        public string ApprovedTime { get; set; }
+        public string AssignedTo { get; set; }
+        public string CreatedTime { get; set; }
+        public string Description { get; set; }
+        public string Comments { get; set; }
+        public string Distance { get; set; }
+        public ClashPoint ClashPoint { get; set; }
+        public string Item1Name { get; set; }
+        public string Item1SourceFile { get; set; }
+        public string Item2Name { get; set; }
+        public string Item2SourceFile { get; set; }
 
-        /* The data type of the clash result representing in the api
-            public Guid Guid { get; set; }
-            public string Name { get; set; }
-            public ClashResultStatus State { get; set; }
-            public string ApprovedBy { get; set; }
-            public DateTime? ApprovedTime { get; set; }
-            public string AssignedTo { get; set; }
-            public DateTime? CreatedTime { get; set; }
-            public string Description { get; set; }
-            public CommentCollection Comments { get; set; }
-            public double Distance { get; set; }
-            public Point3D ClashPoint { get; set; }
-            public ModelItem Item1 { get; set; }
-            public string Item1SourceFile { get; set; }
-            public ModelItem Item2 { get; set; }
-            public string Item2SourceFile { get; set; }
-        */
+        public AClashTestResult(ClashResult clashResult, AClashTest aClashTest)
+        {
+            this.ClashTest = aClashTest;
+            this.Guid = clashResult.Guid;
+            this.Name = clashResult.DisplayName;
+            this.State = GetState(clashResult.Status);
+            this.ApprovedBy = clashResult.ApprovedBy;
+            this.ApprovedTime = clashResult.ApprovedTime.ToString();
+            this.AssignedTo = clashResult.AssignedTo;
+            this.CreatedTime = clashResult.CreatedTime.ToString();
+            this.Description = clashResult.Description;
+            this.Comments = GetClashResultComments(clashResult.Comments);
+            this.Distance = clashResult.Distance.ToString();
+            this.ClashPoint = new ClashPoint(clashResult.Center);
+            this.Item1Name = clashResult.Item1.DisplayName;
+            this.Item1SourceFile = GetItemSourceFile(clashResult.Item1);
+            this.Item2Name = clashResult.Item2.DisplayName;
+            this.Item2SourceFile = GetItemSourceFile(clashResult.Item2);
+        }
 
-        /* The data type of the clash result required for the db
-            public Guid Guid { get; set; }
-            public string Name { get; set; }
-            public string State { get; set; }
-            public string ApprovedBy { get; set; }
-            public string ApprovedTime { get; set; }
-            public string AssignedTo { get; set; }
-            public string CreatedTime { get; set; }
-            public string Description { get; set; }
-            public string Comments { get; set; }
-            public string Distance { get; set; }
-            public ClashPoint ClashPoint { get; set; }
-            public string Item1 { get; set; }
-            public string Item1SourceFile { get; set; }
-            public string Item2 { get; set; }
-            public string Item2SourceFile { get; set; }
-        */
+        private static string GetState(ClashResultStatus clashResultStatus)
+        {
+            switch (clashResultStatus)
+            {
+                case ClashResultStatus.New:
+                    return "New";
+                case ClashResultStatus.Active:
+                    return "Active";
+                case ClashResultStatus.Reviewed:
+                    return "Reviewed";
+                case ClashResultStatus.Approved:
+                    return "Approved";
+                default:
+                    return "Resolved";
+            }
+        }
 
+        private static string GetClashResultComments(CommentCollection commentsCollection)
+        {
+            string comments = string.Empty;
+
+            foreach (Comment comment in commentsCollection)
+            {
+                comments += comment.Body + ", ";
+            }
+
+            return comments;
+        }
+
+        private static string GetItemSourceFile(ModelItem item)
+        {
+            DataProperty property = item.PropertyCategories.FindCategoryByDisplayName("Item").Properties.FindPropertyByDisplayName("Source File");
+            string sourceFile = property.Value.IsDisplayString ? property.Value.ToDisplayString() : property.Value.ToString();
+
+            return sourceFile;
+        }
     }
 }
---
+++ Clash-Management-System-Navisworks-Addin/ViewModels/ClashPoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Autodesk.Navisworks.Api;
+
+namespace Clash_Management_System_Navisworks_Addin.ViewModels
+{
+    public class ClashPoint
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+
+        public ClashPoint(Point3D point)
+        {
+            this.X = (float) point.X;
+            this.Y = (float) point.Y;
+            this.Z = (float) point.Z;
+        }
+    }
+}
