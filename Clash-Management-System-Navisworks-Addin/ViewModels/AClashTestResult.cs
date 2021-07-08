68d94d69aa0f1774dc6e7d523e354cecdf6e696c
Waelyassir <wael.yassir@gmail.com>
2021-07-09 00:08:54 +0200

    edit AClashResult class

--- Clash-Management-System-Navisworks-Addin/ViewModels/AClashTestResult.cs
+++ Clash-Management-System-Navisworks-Addin/ViewModels/AClashTestResult.cs
@@ -16,35 +16,39 @@ namespace Clash_Management_System_Navisworks_Addin.ViewModels
         public string Name { get; set; }
         public string State { get; set; }
         public string ApprovedBy { get; set; }
-        public string ApprovedTime { get; set; }
+        public DateTime? ApprovedTime { get; set; }
         public string AssignedTo { get; set; }
-        public string CreatedTime { get; set; }
+        public DateTime? CreatedTime { get; set; }
         public string Description { get; set; }
         public string Comments { get; set; }
-        public string Distance { get; set; }
+        public double Distance { get; set; }
         public ClashPoint ClashPoint { get; set; }
+        public ModelItem Item1 { get; set; }
         public string Item1Name { get; set; }
         public string Item1SourceFile { get; set; }
+        public ModelItem Item2 { get; set; }
         public string Item2Name { get; set; }
         public string Item2SourceFile { get; set; }
 
-        public AClashTestResult(ClashResult clashResult, AClashTest aClashTest)
+        public AClashTestResult(AClashTest aClashTest, ClashResult clashResult)
         {
             this.ClashTest = aClashTest;
             this.Guid = clashResult.Guid;
             this.Name = clashResult.DisplayName;
             this.State = GetState(clashResult.Status);
             this.ApprovedBy = clashResult.ApprovedBy;
-            this.ApprovedTime = clashResult.ApprovedTime.ToString();
+            this.ApprovedTime = clashResult.ApprovedTime;
             this.AssignedTo = clashResult.AssignedTo;
-            this.CreatedTime = clashResult.CreatedTime.ToString();
+            this.CreatedTime = clashResult.CreatedTime;
             this.Description = clashResult.Description;
             this.Comments = GetClashResultComments(clashResult.Comments);
-            this.Distance = clashResult.Distance.ToString();
+            this.Distance = clashResult.Distance;
             this.ClashPoint = new ClashPoint(clashResult.Center);
+            this.Item1 = clashResult.Item1;
             this.Item1Name = clashResult.Item1.DisplayName;
             this.Item1SourceFile = GetItemSourceFile(clashResult.Item1);
-            this.Item2Name = clashResult.Item2.DisplayName;
+            this.Item2 = clashResult.Item2;
+            this.Item2Name = clashResult.Item2.DisplayName; ;
             this.Item2SourceFile = GetItemSourceFile(clashResult.Item2);
         }
 
