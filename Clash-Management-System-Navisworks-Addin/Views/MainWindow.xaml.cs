c1a830e88d9b3b547496ce228df3258c61631b96
Ahmed Abd Elbary <AhmedAElbary@gmail.com>
2021-08-03 23:53:01 +0200

    Daily Sync

--- Clash-Management-System-Navisworks-Addin/Views/MainWindow.xaml.cs
+++ Clash-Management-System-Navisworks-Addin/Views/MainWindow.xaml.cs
@@ -1,24 +1,15 @@
-﻿using System;
+﻿using Clash_Management_System_Navisworks_Addin.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Interop;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using Clash_Management_System_Navisworks_Addin.ViewModels;
-using Clash_Management_System_Navisworks_Addin.WebService;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Clash_Management_System_Navisworks_Addin.Views
 {
